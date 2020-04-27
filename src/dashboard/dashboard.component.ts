6f38cf10e14c3ecbcf6725302681a595f1449607
lokesh <lokeshsundaresan@gmail.com>
2020-04-27 17:18:56 +0530

    bootstarp setup

---
+++ src/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+
+    selector:'dash-board',
+    templateUrl:'./dashboard.component.html',
+    styleUrls:['./dashboard.component.css']
+
+})
+
+export class DashBoard{
+
+}
