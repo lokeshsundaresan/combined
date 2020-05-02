6c8861d6152e4c63f4c183dc2da30cab3e13924a
lokesh <lokeshsundaresan@gmail.com>
2020-05-02 17:46:45 +0530

    Routes modified

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,19 +1,21 @@
 import { Routes, RouterModule } from "@angular/router";
-
 import { DashBoard } from "../dashboard/dashboard.component";
 import { LoginComponent } from "../pages/login/login.component";
 import { RegisterComponent } from "../pages/register/register.component";
 import { AuthGuard } from "../api_services/user_control/auth.guard";
 
 const routes: Routes = [
-  {
-    path: "",
+  { path: "",
     component: DashBoard,
     canActivate: [AuthGuard],
-    pathMatch: "full"
-  },
-  { path: "login", component: LoginComponent, pathMatch: "full" },
-  { path: "register", component: RegisterComponent, pathMatch: "full" }
+    pathMatch: "full" },
+  { path: "login",
+   component: LoginComponent,
+    pathMatch: "full" },
+  { path: "register",
+    component: RegisterComponent,
+    pathMatch: "full" },
+  { path:"**", redirectTo:''}
 ];
 
 export const appRoutingModule = RouterModule.forRoot(routes);
